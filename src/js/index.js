cd8b9cf696e8cdc5f95965277655ba53ab5b2cbc
OliveiraLucas10 <oliveiraborgeslucas@gmail.com>
2020-05-17 08:31:44 +0100

    added search functionality to search module and expose to controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
 
 /** Global state of the app
  * - Search object
@@ -12,24 +14,27 @@ const state = {
 
 const controlSearch = async () => {
     // 1- get query from view
-    const query = 'pizza'; //TODO:
+    const query = searchView.getInput();
 
     if(query) {
         // 2- new search object and add to state
         state.search = new Search(query);
 
         // 3- prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
 
         // 4- search for recipes
         await state.search.getResults();
 
         // 5- render results on UI
+        searchView.renderResults(state.search.result);
         console.log(state.search.result);
     }
 
 };
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 
