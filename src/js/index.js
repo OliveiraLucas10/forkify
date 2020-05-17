77eb8eaf7086f8b7630053064b7d630c9e27fdfb
OliveiraLucas10 <oliveiraborgeslucas@gmail.com>
2020-05-17 08:58:06 +0100

    added spining loader

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import {elements} from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
  * - Search object
@@ -16,20 +16,21 @@ const controlSearch = async () => {
     // 1- get query from view
     const query = searchView.getInput();
 
-    if(query) {
+    if (query) {
         // 2- new search object and add to state
         state.search = new Search(query);
 
         // 3- prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         // 4- search for recipes
         await state.search.getResults();
 
         // 5- render results on UI
+        clearLoader();
         searchView.renderResults(state.search.result);
-        console.log(state.search.result);
     }
 
 };
