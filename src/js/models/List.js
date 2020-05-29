56eef0449ac98c10b602bd173a27ed64adcfc342
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2020-05-29 23:25:38 +0100

    refactor

--- src/js/models/List.js
+++ src/js/models/List.js
@@ -1,30 +1,30 @@
-import uniqid from 'uniqid';
+import uniqid from "uniqid";
 
 export default class List {
-    constructor() {
-        this.items = [];
-    }
+  constructor() {
+    this.items = [];
+  }
 
-    addItem(count, unit, ingredient) {
-        const item = {
-            id: uniqid(),
-            count,
-            unit,
-            ingredient
-        };
-
-        this.items.push(item);
-        return item;
+  addItem(count, unit, ingredient) {
+    const item = {
+      id: uniqid(),
+      count,
+      unit,
+      ingredient,
     };
 
-    deleteItem(id) {
-        const index = this.items.findIndex(el => el.id === id);
-        // [2, 4, 8] splice(1, 2) [start index, nro items] -> returns [4, 8], original array is [2]
-        // [2, 4, 8] slice(1, 2) [start index, end index (itself not included)] -> returns [4], original array is [2, 4, 8]
-        this.items.splice(index, 1);
-    };
+    this.items.push(item);
+    return item;
+  }
 
-    updateCount(id, newCount) {
-        this.items.find(el => el.id === id).count = newCount;
-    }
-};
+  deleteItem(id) {
+    const index = this.items.findIndex((el) => el.id === id);
+    // [2, 4, 8] splice(1, 2) [start index, nro items] -> returns [4, 8], original array is [2]
+    // [2, 4, 8] slice(1, 2) [start index, end index (itself not included)] -> returns [4], original array is [2, 4, 8]
+    this.items.splice(index, 1);
+  }
+
+  updateCount(id, newCount) {
+    this.items.find((el) => el.id === id).count = newCount;
+  }
+}
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -1,106 +1,123 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default class Recipe {
-    constructor(id) {
-        this.id = id;
+  constructor(id) {
+    this.id = id;
+  }
+
+  async getRecipe() {
+    try {
+      const res = await axios(
+        `https://forkify-api.herokuapp.com/api/get?rId=${this.id}`
+      );
+      this.title = res.data.recipe.title;
+      this.author = res.data.recipe.publisher;
+      this.img = res.data.recipe.image_url;
+      this.url = res.data.recipe.source_url;
+      this.ingredients = res.data.recipe.ingredients;
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    async getRecipe() {
-        try {
-            const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
-            this.title = res.data.recipe.title;
-            this.author = res.data.recipe.publisher;
-            this.img = res.data.recipe.image_url;
-            this.url = res.data.recipe.source_url;
-            this.ingredients = res.data.recipe.ingredients;
+  calcTime() {
+    // assuming that we need 15 min for each 3 ingredients
+    const numIng = this.ingredients.length;
+    const periods = Math.ceil(numIng / 3);
+    this.time = periods * 15;
+  }
 
+  calcServings() {
+    this.servings = 4;
+  }
 
-        } catch (error) {
-            console.log(error);
+  parseIngredients() {
+    const unitsLong = [
+      "tablespoons",
+      "tablespoon",
+      "ounces",
+      "ounce",
+      "teaspoons",
+      "teaspoon",
+      "cup",
+      "pounds",
+    ];
+    const unitsShort = [
+      "tbsp",
+      "tbsp",
+      "oz",
+      "oz",
+      "tsp",
+      "tsp",
+      "cup",
+      "pound",
+    ];
+    const units = [...unitsShort, "kg", "g"];
+
+    const newIngredients = this.ingredients.map((el) => {
+      // 1- uniform units
+      let ingredient = el.toLowerCase();
+      unitsLong.forEach((unit, i) => {
+        ingredient = ingredient.replace(unit, units[i]);
+      });
+
+      // 2- remove parentheses
+      ingredient = ingredient.replace(/ *\([^)]*\) */g, " ");
+
+      // 3- parse ingredients into count, unit and ingredient
+      const arrIng = ingredient.split(" ");
+      const unitIndex = arrIng.findIndex((el2) => units.includes(el2));
+
+      let objIng;
+      if (unitIndex > -1) {
+        // there is a unit
+        // ex. 4 1/2 cups, arrCount is [4, 1/2] --> eval("4+1/2") --> 4.5
+        // ex. 4 cups, arrCount is [4]
+        const arrCount = arrIng.slice(0, unitIndex);
+
+        let count;
+        if (arrCount.length === 1) {
+          count = eval(arrIng[0].replace("-", "+"));
+        } else {
+          count = eval(arrIng.slice(0, unitIndex).join("+"));
         }
-    };
 
-    calcTime() {
-        // assuming that we need 15 min for each 3 ingredients
-        const numIng = this.ingredients.length;
-        const periods = Math.ceil(numIng / 3);
-        this.time = periods * 15;
-    };
+        objIng = {
+          count,
+          unit: arrIng[unitIndex],
+          ingredient: arrIng.slice(unitIndex + 1).join(" "),
+        };
+      } else if (parseInt(arrIng[0], 10)) {
+        // there is NO unit, but 1st element is number
+        objIng = {
+          count: parseInt(arrIng[0], 10),
+          unit: "",
+          ingredient: arrIng.slice(1).join(" "),
+        };
+      } else if (unitIndex === -1) {
+        // there is NO unit and NO number in 1st position
+        objIng = {
+          count: 1,
+          unit: "",
+          ingredient,
+        };
+      }
 
-    calcServings() {
-        this.servings = 4;
-    };
+      return objIng;
+    });
 
-    parseIngredients() {
-        const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cup', 'pounds'];
-        const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
-        const units = [...unitsShort, 'kg', 'g'];
+    this.ingredients = newIngredients;
+  }
 
-        const newIngredients = this.ingredients.map(el => {
-            // 1- uniform units
-            let ingredient = el.toLowerCase();
-            unitsLong.forEach((unit, i) => {
-                ingredient = ingredient.replace(unit, units[i]);
-            });
+  updateServings(type) {
+    // servings
+    const newServings = type === "dec" ? this.servings - 1 : this.servings + 1;
 
-            // 2- remove parentheses
-            ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
+    //ingredients
+    this.ingredients.forEach((ing) => {
+      ing.count *= newServings / this.servings;
+    });
 
-            // 3- parse ingredients into count, unit and ingredient
-            const arrIng = ingredient.split(' ');
-            const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
-
-            let objIng;
-            if (unitIndex > -1) {
-                // there is a unit
-                // ex. 4 1/2 cups, arrCount is [4, 1/2] --> eval("4+1/2") --> 4.5
-                // ex. 4 cups, arrCount is [4] 
-                const arrCount = arrIng.slice(0, unitIndex);
-
-                let count;
-                if (arrCount.length === 1) {
-                    count = eval(arrIng[0].replace('-', '+'));
-                } else {
-                    count = eval(arrIng.slice(0, unitIndex).join('+'));
-                }
-
-                objIng = {
-                    count,
-                    unit: arrIng[unitIndex],
-                    ingredient: arrIng.slice(unitIndex + 1).join(' ')
-                };
-
-            } else if (parseInt(arrIng[0], 10)) {
-                // there is NO unit, but 1st element is number
-                objIng = {
-                    count: parseInt(arrIng[0], 10),
-                    unit: '',
-                    ingredient: arrIng.slice(1).join(' ')
-                };
-            } else if (unitIndex === -1) {
-                // there is NO unit and NO number in 1st position
-                objIng = {
-                    count: 1,
-                    unit: '',
-                    ingredient
-                };
-            }
-
-            return objIng;
-        })
-
-        this.ingredients = newIngredients;
-    };
-
-    updateServings(type){
-        // servings
-        const newServings = type === 'dec' ? this.servings - 1 : this.servings + 1;
-
-        //ingredients
-        this.ingredients.forEach(ing => {
-            ing.count *= (newServings / this.servings);
-        });
-
-        this.servings = newServings;
-    };
-};
+    this.servings = newServings;
+  }
+}
